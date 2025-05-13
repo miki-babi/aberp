04e2fca23a6fc23f03b34fc539236893b3973b58
miki-babi <mikiyasshiferaw@gmail.com>
2025-05-13 13:27:43 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return view('welcome');
+});
+
+
 Route::get('/login', function () {
     return view('Loginpage');
 });
